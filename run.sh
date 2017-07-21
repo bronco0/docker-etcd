ec832e7ed21f010407a1a7c40c51e0b6a0643978
William Teder <wteder@hydreon.com>
2017-07-21 09:16:21 -0700

    Fix binding

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Check for $CLIENT_URLS
 if [ -z ${CLIENT_URLS+x} ]; then
-        CLIENT_URLS="http://0.0.0.0:4001,http://0.0.0.0:2379"
+        CLIENT_URLS="http://etcd:4001,http://etcd:2379"
         echo "Using default CLIENT_URLS ($CLIENT_URLS)"
 else
         echo "Detected new CLIENT_URLS value of $CLIENT_URLS"
@@ -15,15 +15,7 @@ else
         echo "Detected new PEER_URLS value of $PEER_URLS"
 fi
 
-# Check for $CLIENT_IP
-if [ -z ${CLIENT_IP+x} ]; then
-        CLIENT_IP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-        echo "Using default CLIENT_IP ($CLIENT_IP)"
-else
-        echo "Detected new CLIENT_IP value of $CLIENT_IP"
-fi
-
-ETCD_CMD="/bin/etcd -data-dir=/data -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} -advertise-client-urls=http://${CLIENT_IP}:4001 $*"
+ETCD_CMD="/bin/etcd -data-dir=/data -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} -advertise-client-urls=http://etcd:4001 $*"
 echo -e "Running '$ETCD_CMD'\nBEGIN ETCD OUTPUT\n"
 
 exec $ETCD_CMD
